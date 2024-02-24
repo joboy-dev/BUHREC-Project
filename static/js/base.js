2ed20ddfbc648bb4c26f84f769fd2c2264809629
Adegbehingbe, Oluwakorede Joseph <oluwakoredeadegbehingbe@gmail.com>
2024-02-24 15:05:39 +0100

    Admin page UI update

--- static/js/base.js
+++ static/js/base.js
@@ -1,11 +1,9 @@
 const navbar = document.querySelector('nav')
 const navLinks = document.querySelector('nav .nav-links')
-const accountOptions = document.querySelector('.account-options')
+const dropdown = document.querySelector('.dropdown')
 const accountButton = document.querySelector('.account-button')
 
 accountButton.addEventListener('click', () => {
-    navbar.classList.toggle('expand')
-    navLinks.classList.toggle('align-top')
-    accountOptions.classList.toggle('none')
-    accountOptions.classList.toggle('flex')
+    dropdown.classList.toggle('hide-dropdown')
+    dropdown.classList.toggle('show-dropdown')
 })
