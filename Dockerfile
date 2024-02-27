d04fdfeae538756594baf0d02eeda096bd7d2124
Adegbehingbe, Oluwakorede Joseph <oluwakoredeadegbehingbe@gmail.com>
2024-02-27 12:43:42 +0100

    Configured whitenoise for static files

---
+++ Dockerfile
@@ -0,0 +1,29 @@
+FROM python:3.11.5-slim-bullseye
+
+WORKDIR /app
+RUN mkdir staticfiles
+RUN mkdir media
+RUN mkdir media/user
+RUN mkdir media/ckuploads
+
+# install system dependencies
+RUN apt-get update
+
+# copy all files to docker app file
+COPY . /app/
+
+RUN pip install --upgrade pip
+RUN pip install -r requirements.txt
+RUN python manage.py collectstatic --no-input
+RUN python manage.py makemigrations
+RUN python manage.py migrate
+
+# create gunicorn log directory
+RUN mkdir /var/log/gunicorn
+
+ENV PYTHONUNBUFFERED 1
+ENV PYTHONDONTWRITEBYTECODE 1
+
+EXPOSE 8000
+
+CMD [ "gunicorn", "buhrec_project.wsgi"]
--- buhrec_project/settings.py
+++ buhrec_project/settings.py
@@ -48,6 +48,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -127,6 +128,12 @@ USE_TZ = True
 STATIC_URL = 'static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATICFILES_DIRS = [BASE_DIR,'static']
+
+STORAGES = {
+    "staticfiles": {
+        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+    },
+}
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
